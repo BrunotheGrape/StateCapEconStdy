d14de40456e0808d246cfd89a184899dacee04e6
Boyd Ingalls <boyd.ingalls@gmail.com>
2023-08-31 12:01:34 -0500

    Update EconStdyAnalysis.R
    Reading state level data for the Information Sector

--- EconStdyAnalysis.R
+++ EconStdyAnalysis.R
@@ -9,3 +9,20 @@ ANYInfo <- c(t(AlbanyNyInformation[,-1]))
 ANYInfo <- ANYInfo[1:125]
 
 Res <- cor(LRARInfo,ANYInfo)
+
+
+CLF <- rbind(AlbanyNyCivilianLaborForce$Mar[11], AtlantaGaCivilianLaborForce$Mar[11]
+             ,AustinTxCivilianLaborForce$Mar[11]
+             ,BatonRougeLaCivilianLaborForce$Mar[11], BismarkNdCivilianLaborForce$Mar[11]
+             ,BoiseIdCivilianLaborForce$Mar[11], BostonMaMetroCivilianLaborForce$Mar[11]
+             ,ColumbiaScCivilianLaborForce$Mar[11], ColumbusOhCivilianLaborForce$Mar[11]
+             ,DenverCoCivilianLaborForce$Mar[11]
+             ,DesMoinesIaCivilianLaborForce$Mar[11], DoverDeCivilianLaborForce$Mar[11]
+             ,HarrisburgPaCivilianLaborForce$Mar[11], HartfordCtCivilianLaborForce$Mar[11]
+             ,IndianapolisInCivilianLaborForce$Mar[11], JacksonMsCivilianLaborForce$Mar[11]
+             ,LittleRockArCivilianLaborForce$Mar[11])
+
+sumCLF <- summary(CLF)
+print(sumCLF)
+
+#Emp <- AlbanyNyCivilianLaborForce
