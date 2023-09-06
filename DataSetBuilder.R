0df0797b7248edd67c68517adfe4d8a9af4915ff
Boyd Ingalls <boyd.ingalls@gmail.com>
2023-09-06 10:38:25 -0500

    Update DataSetBuilder.R

--- DataSetBuilder.R
+++ DataSetBuilder.R
@@ -1,13 +1,24 @@
 # Econ Study data set builder
 
-datalst <- list('Manufacturing', 'TradeTransportationUtilities', 'Information'
-                ,'Financial', 'ProfessionalandBusinessServices', 'EducationandHealthServices'
-                ,'LeisureHospitality', 'OtherServices','Government')
+
+# SprgfldIl <- rbind(c(t(SpringfieldIlEducationandHealthServices[,-1])),c(t(SpringfieldIlFinancial[,-1])),c(t(SpringfieldIlGovernment[,-1]))
+#                   ,c(t(SpringfieldIlInformation[,-1])), c(t(SpringfieldIlManufacturing[,-1])), c(t(SpringfieldIlOtherServices[,-1]))
+#                   ,c(t(SpringfieldIlLeisureHospitality[,-1])),c(t(SpringfieldIlProfessionalandBusinessServices[,-1])), c(t(SpringfieldIlTradeTransportationUtilities[,-1])))
+
+#write.csv(SprgfldIl,"SprgfildIl.csv")
 
 
 
-SprgfldIl <- rbind(c(t(SpringfieldIlEducationandHealthServices[,-1])),c(t(SpringfieldIlFinancial[,-1])),c(t(SpringfieldIlGovernment[,-1]))
-                  ,c(t(SpringfieldIlInformation[,-1])), c(t(SpringfieldIlManufacturing[,-1])), c(t(SpringfieldIlOtherServices[,-1]))
-                  ,c(t(SpringfieldIlLeisureHospitality[,-1])),c(t(SpringfieldIlProfessionalandBusinessServices[,-1])), c(t(SpringfieldIlTradeTransportationUtilities[,-1])))
+region <- 'SpringfieldIl'
+sector <- c('EducationandHealthServices', 'Financial', 'Government', 'Information'
+            ,'Manufacturing', 'OtherServices', 'LeisureHospitality', 'ProfessionalandBusinessServices'
+            ,'TradeTransportationUtilities')
 
-write.csv(SprgfldIl,"SprgfildIl.csv")
+
+SprgfldIl <- data.frame()
+
+
+for (sector in sector) {
+  output <- c(t(get((paste0(noquote(region),noquote(sector))))[,-1]))
+  SprgfldIl <- rbind(SprgfldIl, output)
+}
