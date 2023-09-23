2cdbe73c83dcc0a466602e96c8a5143078d31651
Boyd Ingalls <boyd.ingalls@gmail.com>
2023-09-23 16:28:02 -0500

    Update DataSetBuilder.R
    Create dataset to be converted to time series for Civilian Labor Force

--- DataSetBuilder.R
+++ DataSetBuilder.R
@@ -23,3 +23,47 @@ for (sector in sector) {
   SprgfldIl <- rbind(SprgfldIl, output)
 }
 SprgfldIl <- SprgfldIl[,1:125]
+
+# CapMnCivLabr <- mean(c(AlbanyNyInformation, AtlantaGaInformation,AustinTxInformation, BatonRougeLaInformation, BismarkNdInformation, BoiseIdInformation, BostonMaMetroInformation, CharlestonWvInformation
+#                        ,CheyenneWyInformation, ColumbiaScInformation, ColumbusOhInformation, DenverCoInformation,DesMoinesIaInformation, DoverDeInformation, HarrisburgPaInformation
+#                        ,DesMoinesIaInformation,DoverDeInformation, HarrisburgPaInformation, HartfordCtInformation, IndianapolisInInformation,JacksonMsInformation
+#                        ,LansingMiInformation,LincolnNeInformation, LittleRockArInformation,MadisonWiInformation, MinneapolisMnInformation, MontgomeryAlInformation
+#                        ,NashvilleTnInformation, OklahomaCityOkInformation, PhoenixAzInformation, ProvidenceRiInformation, RaleighNcInformation, RichmondVaInformation
+#                        , SacramentoCaInformation, SalemOrInformation, SaltLakeCityUtInformation, SantaFeNmInformation, SpringfieldIlInformation, TallahasseeFlInformation
+#                        ,TopekaKsInformation, TrentonNjInformation))
+
+
+CivLbrFrc <- data.frame
+#output <- data.frame
+sector <- c('EducationandHealthServices', 'Financial')
+
+region <- 'SpringfieldIl'
+for (region in region) {
+  output <- c(t(get((paste0(noquote(region),noquote(sector))))[,-1]))
+  CivLbrFrc <- rbind(CivLbrFrc, output)
+  #CivLbrFrc <- output
+}
+
+region <- c('AlbanyNy', 'AtlantaGa','AustinTx', 'BatonRougeLa', 'BismarkNd', 'BoiseId', 'BostonMaMetro', 'CharlestonWv'
+              ,'CheyenneWy', 'ColumbiaSc', 'ColumbusOh', 'DenverCo','DesMoinesIa', 'DoverDe', 'HarrisburgPa'
+              ,'DesMoinesIa', 'DoverDe', 'HarrisburgPa', 'HartfordCt', 'IndianapolisIn','JacksonMs'
+              ,'LansingMi','LincolnNe', 'LittleRockAr','MadisonWi', 'MinneapolisMn', 'MontgomeryAl'
+              ,'NashvilleTn', 'OklahomaCityOk', 'PhoenixAz', 'ProvidenceRi', 'RaleighNc', 'RichmondVa'
+              , 'SacramentoCa', 'SalemOr', 'SaltLakeCityUt', 'SantaFeNm', 'SpringfieldIl', 'TallahasseeFl'
+              ,'TopekaKs', 'TrentonNj')
+sector <- c('CivilianLaborForce')
+
+
+CivLbrFrc <- data.frame()
+
+
+for (region in region ) {
+  output <- c(t(get((paste0(noquote(region),noquote(sector))))[,-1]))
+  CivLbrFrc <- rbind(CivLbrFrc, output)
+}
+CivLbrFrc <- CivLbrFrc[,1:125]
+
+
+CivLbrFrc
+
+
